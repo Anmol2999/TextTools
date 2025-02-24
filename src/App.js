1f835ae32f38b25f1b83706f7c00c91f454a2ee1
Anmol Gupta <AnmolGupta2999@gmail.com>
2025-02-24 19:29:45 +0530

    fixed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+// import About from './components/About.js';
+import Form from './components/Form.js';
+import Navbar from './components/Navbar.js';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  const[mode,setmode]=useState("light") //whether the dark mode is on or not
+
+  const toggleMode=()=>{
+    // console.log("working")
+      if(mode==="light")
+        {
+      setmode("dark");
+      document.body.style.backgroundColor="rgb(4 1 56)"
+    }
+    else{
+      setmode("light");
+      document.body.style.backgroundColor="white"
+
+    }
+
+  }   
+  return (  
+    <>
+      <Navbar title="TextTools" mode={mode} toggleMode={toggleMode}/> 
+      <div className='container my-3' >
+        <Form mode={mode} heading="Enter text"/>
+        {/* <About/> */}
+        </div>     
+    </>
   );
 }
 
---
+++ src/components/Form.js
@@ -0,0 +1,46 @@
+import React ,{useState}from 'react'
+
+//useState is the type of hook in react
+//States make site dynamic
+
+export default function Form(props) {
+    const [text,setText]=useState("This is text")
+    const handleonchange =(event)=>{
+        //console.log("text changed")
+        setText(event.target.value)
+    }
+   const handleonclick1 =()=>{
+    let newtext=text.toUpperCase();
+        setText(newtext)
+   }
+
+   const handleonclick2 =()=>{
+    let newtext=text.toLowerCase();
+    //text="newtext" wrong way to change state
+        setText(newtext)//correct way to change the state
+   }
+  return (
+    <><div style={{color : props.mode==="light"?"black":"white"}}> 
+    <h1>{props.heading}</h1>
+    <div className="mb-3 " >
+    <textarea className="form-control" value={text} style={{backgroundColor : props.mode==="dark"?"grey":"white",
+        color: props.mode==="dark"?"white":"black"}}onChange={handleonchange} id="exampleFormControlTextarea1" rows="3"></textarea>
+    <button type="submit" className="btn btn-primary my-3"  onClick={handleonclick1}>UpperCase</button>
+    <button type="submit" className="btn btn-primary mx-3"  onClick={handleonclick2}>LowerCase</button>
+    </div>
+
+    <div>
+        <h2 >Text Summary</h2>
+        <p>{text.split(" ").length} word and {text.length} characters</p>
+        <p>{0.008*text.split(" ").length} minutes read</p>
+
+    </div>
+</div>
+   
+
+   
+   
+    </>
+    
+  )
+}
